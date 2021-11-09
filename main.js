18e66eafcc7f4cf80c01f2f1547e9407015a68b8
lumilio <logistic-division@lumilium.com>
2021-11-09 20:59:52 +0100

    esercizio terminato

--- main.js
+++ main.js
@@ -18,15 +18,26 @@ const app = new Vue({
     el: '#root',
 
     data: {
+        emails: [],
+        error: "",
     },
 
     methods: { 
     },
 
     mounted(){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(myResp => {
-            console.log(myResp);
-            this.emails.push(myResp.data.response)}).catch(e => {console.error(e, "OPS!"); this.error = e})
+
+        for (let index = 0; index < 10; index++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(myResp => {
+                console.log(myResp);
+                this.emails.push(myResp.data.response)}).catch(e => {console.error(e, "OPS!"); this.error = e;
+            })
+        }
+
+
+
+        console.log(this.emails);
+        console.log(this.error);
     }
 
 })
